4ddd0a1e76865bdb2900eae5bec943ef0eb05ef2
Christopher Marshall <rookiegamedev@gmail.com>
2018-06-20 16:09:37 +0100

    server runs.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const app = require('express')();
+const server = require("express");
+const app = server();
 const parser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
-app.use(parser.json(), parser.urlencoded({ extended: true }), app.static('client/build'));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(server.static("client/build"));
+
 
 MongoClient.connect("mongodb://localhost:27017", (err, client) => {
     if (err) { console.log(); return; }
@@ -10,7 +14,13 @@ MongoClient.connect("mongodb://localhost:27017", (err, client) => {
     const db = client.db("bucket_list");
     console.log("Connected to database!!");
 
-    server.get('/api/bucketlist', (req, res) => {
-        
+    app.get('/api/bucketlist', (req, res) => {
+        db.collection('list').find().toArray((err, allList) => {
+            if (err) console.log(err); 
+        });
+    });
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
     });
 });
